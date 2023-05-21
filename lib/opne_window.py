485e763bd7575facc6b77fa7dcbe2dc9ca98d678
newkincode <maainnewkin59@gmail.com>
2023-05-21 11:27:24 +0900

    no message

--- lib/opne_window.py
+++ lib/opne_window.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 
 root = tk.Tk()
-# root.attributes('-alpha', 0.3)  # 앱 전체가 투명해짐
 root.wm_attributes("-transparentcolor", "white")	# 흰색을 투명하도록 지정
 root.wm_attributes("-topmost", 1)
-
-canvas = tk.Canvas(root, width=600, height=300, bg='white')	# 배경을 흰색으로 지정
+root.attributes("-fullscreen",not root.attributes("-fullscreen"))
+monitor_height = root.winfo_screenheight()
+monitor_width = root.winfo_screenwidth()
+canvas = tk.Canvas(root, width=monitor_width, height=monitor_height, bg='white')	# 배경을 흰색으로 지정
 canvas.grid(columnspan=3, rowspan=3)
-instructions = tk.Label(root, text="Hello World", font="Raleway")
+instructions = tk.Label(root,wraplength=1000, text="HelloSDAFSDFASFASDASDFASDFADFASDFDSASDFSDSAFADSFASDFASDFASDFADSFASDFADSFASDFADFASDFASDFASDFASDFASDFASDFSDFASDFASDFADWSFSDSDFDSFASDASDFASDFASDFSDFASDFDFASDFASDFASDASDFD World", font="Raleway")
 instructions.grid(columnspan=3, column=0, row=0)
 
 root.mainloop()
